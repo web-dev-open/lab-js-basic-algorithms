d9f04740aad94bf267afa0900d72302b19809209
arshmohd <irfan.arshad4@gmail.com>
2022-08-29 16:33:20 +0530

    Finished iteration-1 for day-1 is done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // Iteration 1: Names and Input
+
+let hacker1 = "Mohammed";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Manish";
+console.log(`The navigator's name is ${hacker2}`);
 //
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
