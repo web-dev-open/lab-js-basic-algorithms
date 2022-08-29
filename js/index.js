5e5b880757c0daf264a2577f47bb2e88d84be8e1
arshmohd <irfan.arshad4@gmail.com>
2022-08-29 19:46:55 +0530

    Finished the Bonus-1 1st task finished for day-1

--- js/index.js
+++ js/index.js
@@ -57,3 +57,17 @@ if (hacker1 > hacker2) {
 } else {
   console.log(`What?! You both have the same name?`);
 }
+
+//Bonues-1
+
+let loremStrPara = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis sem posuere, pellentesque massa a, gravida massa. Fusce sagittis ex sed vehicula dignissim. Duis laoreet neque quis mi feugiat rutrum nec sed nibh. Suspendisse rhoncus suscipit cursus. Proin convallis urna at quam posuere dapibus. Ut pharetra elit eget urna semper, id mollis justo euismod. Maecenas auctor maximus ante vel sodales. Nam consectetur tincidunt lectus, vitae mollis erat dapibus quis. Vestibulum consectetur, arcu non blandit ultrices, mauris libero consectetur velit, ac sodales tellus ipsum sed leo. Sed in accumsan nisi. Curabitur maximus in odio gravida lacinia. Vestibulum dapibus egestas dui vel finibus. Phasellus est elit, vestibulum a turpis eget, eleifend pharetra dolor. Nulla fringilla, tortor nec vestibulum blandit, diam nibh hendrerit tellus, sed tempor elit ex non eros. Cras varius sem et aliquam faucibus.
+
+Curabitur ultrices facilisis turpis, eu viverra nunc rhoncus quis. Aenean gravida, nunc at dictum placerat, augue felis imperdiet urna, a sollicitudin augue massa eu mauris. Nulla nec gravida eros. Nulla vestibulum nisi eget lacinia dapibus. Donec molestie semper libero at sodales. Vestibulum bibendum, dui sed congue vestibulum, massa magna lacinia quam, eleifend feugiat odio nulla id urna. Suspendisse ullamcorper nec tellus vitae cursus. Pellentesque cursus sem lectus, et commodo sem aliquam at. Fusce vel sem urna. Mauris venenatis sed tortor ut tristique. Phasellus quis bibendum magna.
+
+Donec tempor, diam vel dapibus fringilla, justo mi consectetur eros, vitae commodo ligula diam a ante. Sed porttitor arcu neque, ut aliquam ligula bibendum quis. Duis quis odio vel neque cursus malesuada id eu sapien. Vestibulum hendrerit dapibus nulla, mollis blandit odio malesuada ac. Nullam pellentesque, mi accumsan aliquet consequat, eros elit pharetra enim, a mollis dolor nisi quis augue. Etiam at tellus sed mauris iaculis porta. Aliquam metus augue, ultrices a dolor eu, auctor rutrum elit. Vestibulum aliquet est quis mollis tristique. Proin pulvinar varius dignissim. Aenean in augue et purus auctor pharetra quis rhoncus velit.`;
+
+let countWords = givenString => {
+  return givenString.split(" ").length;
+};
+
+console.log(countWords(loremStrPara));
