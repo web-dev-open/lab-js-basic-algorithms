e2e6f670fe98151b7440702ad3fb591fe90178c2
unknown <essaadihamza3@gmail.com>
2023-09-04 11:39:06 +0100

    done

--- js/index.js
+++ js/index.js
@@ -20,3 +20,44 @@ if (driverNameLength > navigatorNameLength) {
 }
 
 // Iteration 3: Loops
+let NameSeparated = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  NameSeparated += hacker1.charAt(i).toUpperCase() + " ";
+}
+
+console.log(NameSeparated);
+
+
+let NameReversed= "";
+
+for (let i = hacker1.length -1 ; i >=0;i--){
+  NameReversed += hacker1[i] ;
+}
+
+console.log(NameReversed);
+
+
+
+let result = "";
+for (let i = 0; i < Math.min(hacker1.length, hacker2.length); i++) {
+    if (hacker1[i] < hacker2[i]) {
+        result = "The driver's name goes first.";
+        break;
+    } else if (hacker1[i] > hacker2[i]) {
+        result = "Yo, the navigator goes first definitely.";
+        break;
+    }
+}
+
+if (result === "") {
+    if (hacker1.length < hacker2.length) {
+        result = "The driver's name goes first.";
+    } else if (hacker1.length > hacker2.length) {
+        result = "Yo, the navigator goes first definitely.";
+    } else {
+        result = "What?! You both have the same name?";
+    }
+}
+
+console.log(result);
