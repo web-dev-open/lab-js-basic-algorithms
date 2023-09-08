a0569ef7f2ada95dcc718af8480f3e7c17a9699d
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-09-08 13:06:56 +0100

    Finish Iteration 3

--- js/index.js
+++ js/index.js
@@ -21,3 +21,22 @@ if (driverNameIsLonger) {
 }
 
 // Iteration 3: Loops
+let formattedDriverName = '';
+for (let i = 0; i < hacker1.length; i++) {
+  formattedDriverName += `${hacker1.charAt(i).toUpperCase()} `;
+}
+console.log(formattedDriverName.trim());
+
+let reversedNavigatorName = '';
+for (let i = hacker2.length; i >= 0; i--) {
+  reversedNavigatorName += hacker2.charAt(i);
+}
+console.log(reversedNavigatorName);
+
+if (hacker1.toLowerCase() < hacker2.toLowerCase()) {
+  console.log("The driver's name goes first.");
+} else if (hacker1.toLowerCase() > hacker2.toLowerCase()) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
