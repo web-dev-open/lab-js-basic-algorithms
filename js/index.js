68833a33811771aea564ed47d7e2305119e74492
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-09-11 14:45:48 +0100

    Finish Bonus 1

--- js/index.js
+++ js/index.js
@@ -40,3 +40,23 @@ if (hacker1.toLowerCase() < hacker2.toLowerCase()) {
 } else {
   console.log("What?! You both have the same name?");
 }
+
+// Bonus 1
+const loremParagraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum laoreet odio, vitae ultrices neque volutpat ac. Proin vestibulum nisi id ultricies rhoncus. Sed non nunc arcu. Duis ut dui nulla. Nunc commodo ac ipsum ac condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam aliquam ante eu ipsum pretium ullamcorper. Aliquam non venenatis ante, vel posuere lorem. Sed id orci quis est sollicitudin cursus. Curabitur lectus augue, accumsan quis mi a, vehicula maximus nisi. Aenean a vehicula nulla. Vestibulum leo orci, lobortis ut pharetra eu, ultrices at nibh. Duis feugiat tincidunt tempor. Suspendisse potenti. Nulla justo elit, consequat id ultricies quis, commodo eu erat. Ut sodales iaculis vulputate.
+
+Mauris mauris sem, rhoncus eu egestas ac, viverra sit amet augue. Mauris velit tortor, suscipit ultricies nisi vitae, dapibus volutpat justo. Proin consequat vehicula commodo. Ut eleifend tincidunt ipsum sed sodales. Maecenas ante tortor, cursus id fermentum vitae, fringilla quis massa. Proin sed sapien sem. Duis mattis at quam vitae interdum. Cras ligula risus, aliquet et dictum pharetra, mollis in tortor. Praesent in urna vel nisi elementum placerat eu et justo. Aliquam ac ante eget eros consequat rhoncus. Nullam id nisl dui. Suspendisse potenti. Suspendisse blandit, nunc eget tristique laoreet, purus neque vestibulum nisl, a dignissim justo orci ac magna. Nulla nisl risus, volutpat ac pretium non, lacinia sed metus. Proin pharetra feugiat metus id suscipit. Sed tellus ligula, dapibus id mollis feugiat, sagittis sollicitudin purus.
+
+Donec sollicitudin orci neque, vitae congue dolor molestie eget. Fusce ultrices est id mauris interdum, sed finibus est varius. Sed malesuada ultrices massa eu mollis. Nulla sit amet turpis eget lacus sollicitudin vehicula. Nulla cursus commodo sapien non posuere. Etiam ac orci tincidunt, faucibus arcu vel, accumsan turpis. Aenean bibendum fermentum sapien non placerat. Cras porta, velit ac varius ornare, mi elit eleifend nunc, accumsan vulputate nulla quam vitae purus. Maecenas feugiat volutpat felis eu sollicitudin. Donec mattis placerat velit. Nunc sed felis porttitor lacus consectetur rutrum nec quis ipsum. Sed quis placerat arcu. Etiam dignissim magna eget semper gravida. Donec vitae tincidunt ex. Ut vulputate orci tortor, sed rhoncus augue cursus vitae. Fusce sit amet venenatis ipsum.`;
+
+let wordCount = 0;
+const loremArray = loremParagraphs.split(' ');
+for (let i = 0; i < loremArray.length; i++) {
+  const loremWord = loremArray[i];
+  if (loremWord.includes('\n')) {
+    wordCount += 2;
+  } else {
+    wordCount++;
+  }
+}
+
+console.log(wordCount);
