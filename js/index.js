f52b7a0684951226f553f280d9f15508dc755fcf
Manoj kashyap <manojk9788@gmail.com>
2023-09-29 22:46:49 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
+
+console.log("I'm ready!");
 // Iteration 1: Names and Input
 //
+// A variable hacker1 with the driver's name.
+const hacker1 = "Manoj Kashyap"; // 
+
+//Print "The driver's name is XXXX".
+console.log(`The driver's name is ${hacker1}`);
+
+//Create a variable hacker2 with the navigator's name.
+const hacker2 = "Manish poduval"; // 
+
+//Print "The navigator's name is YYYY".
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
