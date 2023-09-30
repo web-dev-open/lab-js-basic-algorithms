25592f3f201f2879a94f339d8c5cae7a63434f64
AleleChi <alelechi17@gmail.com>
2023-09-30 22:45:01 +0100

    declared the second variable

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@
 
 // Iteration 3: Loops
 
-
+// variable with driver's name
 let hacker1 = "Joe Doe";
-console.log()
+console.log(`The driver's name is ${hacker1}`);
+
+//variable with navigator's name
+
+let hacker2 = "Dera Bide";
+console.log(`The navigator's name is ${hacker2}`);
