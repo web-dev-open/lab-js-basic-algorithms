75f7a95acf4dd4cbab5a2849cf4c7342bdba227d
AleleChi <alelechi17@gmail.com>
2023-10-02 09:27:58 +0100

    Additional changes

--- js/index.js
+++ js/index.js
@@ -6,10 +6,20 @@
 // Iteration 3: Loops
 
 // variable with driver's name
-let hacker1 = "Joe Doe";
+let hacker1 = "Dera";
 console.log(`The driver's name is ${hacker1}`);
 
 //variable with navigator's name
 
-let hacker2 = "Dera Bide";
+let hacker2 = "Dera";
 console.log(`The navigator's name is ${hacker2}`);
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name and its has ${hacker1.length} characters`)
+} if(hacker1 == hacker2){
+    console.log(`Wow, you both have equally long names, ${hacker1.length}`);
+}
+
+else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+}
