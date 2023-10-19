009ca7a09617d70e893ece81bb3a9efc7c28ba42
SushmithaSudarshan <sushmithasudarshan515@gmail.com>
2023-10-19 21:19:59 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
+
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+
+// Create a variable hacker1 with the driver's name
+const hacker1 = "Sushmitha Sudarshan";
+
+// Print "The driver's name is XXXX"
+console.log(`The driver's name is ${hacker1}`);
+
+//Create a variable hacker2 with the navigator's name
+const hacker2 = "Manish Poduval";
+
+//Print "The navigator's name is YYYY"
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
