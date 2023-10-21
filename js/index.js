79b68dbab0ce726493973d3e99fd7613eaca6757
mahesh878 <donthumahesh1@gmail.com>
2023-10-21 11:26:24 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Iteration 1: Names and Input
 
-let hacker1 = "Mahesh";
-console.log("The driver's name is",hacker1);
+let hacker1 = `Mahesh`;
+console.log(`The driver's name is ${hacker1}.`);
 
-let hacker2 ="manoj";
-console.log("The navigator's name is",hacker2);
+let hacker2 =`manoj`;
+console.log(`The navigator's name is ${hacker2}.`);
 
 
 
