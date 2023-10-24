8a475d1fc7555eb484037879d33bc0eb34c31623
hackwell <hackwell.4216@gmail.com>
2023-10-24 10:31:06 +0000

    complete iteration two and three - Conditionals & Loops

--- js/index.js
+++ js/index.js
@@ -6,5 +6,34 @@ const hacker2 = "Enoch";
 console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
+if (hacker1.length > hacker2.length)
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+else if (hacker2.length > hacker1.length)
+  console.log(
+    `It sees that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+else
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length || hacker2.length}`
+  );
 
 // Iteration 3: Loops
+//printing driver's name separated by space andin capitals
+let driverNameInCapsWithSpace = "";
+for (const letter of hacker1) {
+  driverNameInCapsWithSpace += letter.toUpperCase() + " ";
+}
+
+console.log("Driver's name in Caps: " + driverNameInCapsWithSpace);
+
+// printing navigator's name in reverse order
+let navigatorNameInReverse = hacker2.split("").reverse().join("");
+console.log(navigatorNameInReverse);
+
+// ordering names
+if (hacker1 > hacker2) console.log("The driver's name goes first.");
+else if (hacker1 < hacker2)
+  console.log("Yo, the navigator goes first definitely.");
+else console.log("What?! You both have the same name?");
