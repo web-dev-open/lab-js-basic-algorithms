efd2923dfdb278c3f6d5881f29cd2cda21fceab4
Official-Lovely-kumari <officiallovelykumari@gmail.com>
2023-11-03 15:50:34 +0530

    adding names and input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-// Iteration 1: Names and Input
-//
+ // Iteration 1: Names and Input
+let hacker1=`Ronny`;
+console.log(`The Driver's name is ${hacker1}. `);
+let hacker2=`Sam`;
+console.log(` The Navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
 
 
