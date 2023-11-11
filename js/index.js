145477d79c6f6da07b57b9b8de6a4bef961bbc16
ChathuraHasanga44 <s92067130@ousl.lk>
2023-11-11 19:25:50 +0530

    done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ console.log("I'm ready!");
 
 // Iteration 1: Names and Input
 //
+let hacker1 = "Chathura";
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "Abeywickrama";
+console.log("The navigator's name is " + hacker2);
 // Iteration 2: Conditionals
 
 
