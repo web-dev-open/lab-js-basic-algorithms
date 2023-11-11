7f4f83cba1c7a645b1e9d093bbff8d18f9a579c9
ChathuraHasanga44 <s92067130@ousl.lk>
2023-11-11 21:34:36 +0530

    done

--- js/index.js
+++ js/index.js
@@ -15,9 +15,33 @@ if(hacker1.length> hacker2.length){
 
 }else if(hacker2.length>hacker1.length){
     console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters.");
-    
+
 }else{
     console.log("Wow, you both have equally long names, " + hacker2.length + " characters!");
 }
 
 // Iteration 3: Loops
+//task 3.1
+let result =" ";
+for(let i=0; i<hacker1.length; i++){
+    result += hacker1[i].toUpperCase() + " ";
+}
+console.log(result.trim());
+
+//task 3.2
+let reverse = " ";
+for(let i=hacker2.length-1; i>= 0; i--){
+    reverse += hacker2[i];
+}
+console.log(reverse);
+
+//task 3.3
+if (hacker1.localeCompare(hacker2) < 0) {
+    console.log("The driver's name goes first.");
+} else if (hacker1.localeCompare(hacker2) > 0) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
+
+
