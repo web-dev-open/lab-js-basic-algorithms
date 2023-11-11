d58a05913b3294b6c2920027706b9979d9975865
ChathuraHasanga44 <s92067130@ousl.lk>
2023-11-11 22:49:26 +0530

    done

--- js/index.js
+++ js/index.js
@@ -44,4 +44,26 @@ if (hacker1.localeCompare(hacker2) < 0) {
     console.log("What?! You both have the same name?");
 }
 
+//Bonus time!
+//Bonus 1
+const para1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const para2 ="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+const para3 ="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 
+const wordCount1 = para1.split(' ').length;
+console.log(wordCount1);
+
+const wordCount2 = para2.split(' ').length;
+console.log(wordCount2);
+
+const wordCount3 = para3.split(' ').length;
+console.log(wordCount3);
+
+const etCount1 = (para1.match(/\bet\b/g) || []).length;
+console.log("Number of occurrences of 'et':", etCount1);
+
+const etCount2 = (para2.match(/\bet\b/g) || []).length;
+console.log("Number of occurrences of 'et':", etCount2);
+
+const etCount3 = (para3.match(/\bet\b/g) || []).length;
+console.log("Number of occurrences of 'et':", etCount3);
