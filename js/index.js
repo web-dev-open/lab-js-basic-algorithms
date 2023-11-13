5f5f97a2ac0c613519e9a12a14b3e38c68b9b5f2
M Sami <beboolean@gmail.com>
2023-11-13 18:41:41 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+let hacker1 = "bootpen";
+console.log("The driver's name is " + hacker1);
+let hacker2 = "pencamp";
+console.log("The navigator's name is " + hacker2);
+
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log( "The driver has the longest name, it has " + hacker1.length + " characters.");
+} else if (hacker1.length === hacker2.length) {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+} else {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.` );
+}
 
 // Iteration 3: Loops
