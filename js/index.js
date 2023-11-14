41bfcacee7bab0d599a3c7d5ed96f9075ed2c99c
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-11-14 15:25:29 +0800

    done iteration1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = "John";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Jane";
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
