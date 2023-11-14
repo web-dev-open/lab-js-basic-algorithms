317d8e765b55d6d477a2ad3aa2f6a320acee62da
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-11-14 16:02:58 +0800

    done iteration 3

--- js/index.js
+++ js/index.js
@@ -13,3 +13,14 @@ if (hacker1.length > hacker2.length) {
   console.log(`Wow, you both have equally long names, ${hacker1.length + hacker2.length} characters`);
 }
 // Iteration 3: Loops
+let splitHacker1 = hacker1.split("").join(" ").toUpperCase();
+console.log(splitHacker1);
+
+let reverseHacker2 = hacker2.split("").reverse().join("");
+console.log(reverseHacker2);
+
+if (hacker1 < hacker2) console.log(`The driver's name goes first.`);
+else if (hacker2 < hacker1) console.log(`Yo, the navigator goes first definitely.`);
+else {
+  console.log(`What?! You both have the same name?`);
+}
