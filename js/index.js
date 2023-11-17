b3b2add8a679f21b8551b954eb59f0763f713056
blindaks <akrativerma2002@gmail.com>
2023-11-17 23:54:46 +0530

    First iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-//
+let hacker1="Ajay";
+console.log("The driver's name is "+hacker1);
+let navigator1="AK";
+console.log("The navigator's name is "+navigator1);
 // Iteration 2: Conditionals
 
 
