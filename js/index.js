39c6166e6eed6203bc8110ea70a7d92f2942ef99
Rishi Damle <108988060+Rishi-Damle@users.noreply.github.com>
2023-11-17 04:16:18 -0800

    itreation 2 is completed

--- js/index.js
+++ js/index.js
@@ -6,7 +6,18 @@ let hacker2="Prajval";
 console.log("The navigator's name is"+hacker2+".");
 //
 // Iteration 2: Conditionals
-
+let hacker1NameLength=hacker1.length;
+let hacker2NameLength=hacker2.length;
+if(hacker1NameLength>hacker2NameLength)
+{
+    console.log('The hacker1 has the longest name, it has ${hacker1NameLength} characters.');  
+}
+else if (hacker2NameLength>hacker1NameLength) {
+    console.log('The hacker2 has the longest name, it has ${hacker2NameLength} characters.');     
+}
+else{
+    console.log(`Wow, you both have equally long names, characters!`);
+}
 console.log("I'm ready!");
 
 
