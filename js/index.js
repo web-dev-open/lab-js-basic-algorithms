7f3c887a03789c3801d8140969f87ffe5965f1a1
PriyankaDandupati23 <priyadandu10@gmail.com>
2023-11-30 11:25:49 +0530

    Iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-//
+hacker1='XXXX'
+console.log("The driver's name is XXXX")
+hacker2='YYYY'
+console.log("The navigator's name is YYYY")
 // Iteration 2: Conditionals
 
 
