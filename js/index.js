e2e23808744cf24e5e0f870a552b0b17236cadfb
Aditya Koche <130300872+AdityaKoche01@users.noreply.github.com>
2024-01-19 23:14:51 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const hacker1 = "John";
 console.log(`The driver's name is ${hacker1}`);
 const hacker2 = "Jane";
 console.log(`The navigator's name is ${hacker2}`);
+
 //
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
