9c404106f9eff06272c913859ac07eef0943a752
Ragini Deshpande <rags6167@gmail.com>
2024-03-10 17:18:15 +0530

    variables defined

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
-console.log("I m ready!");
-console.log("new change");
+// console.log("I m ready!");
+// console.log("new change");
+
+let hacker1 = "vijay";
+console.log(`"The driver's name is ${hacker1}."`);
+let hacker2 = "rana";
+console.log(`"The navigator's name is ${hacker2}."`);
+
 // Iteration 2: Conditionals
 
 
