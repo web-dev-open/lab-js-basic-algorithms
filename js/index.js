58ee72d0fcd03f6c2bcc3749417771a1f8de11fb
CodeBite <giovaniportal@gmail.com>
2024-03-15 14:57:06 -0700

    finished iteration 1
    - solution tested

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
 
+let hacker1 = "John Doe";
+console.log(`The driver's name is ${hacker1}`);
 
-// Iteration 3: Loops
+let hacker2 = "Jane Doe";
+console.log(`The navigator's name is ${hacker2}`);
